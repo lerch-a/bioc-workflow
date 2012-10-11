1345772da3b656c7186cf1f9dd278fd2480479f3
user@server <user@server>
2012-10-11 16:02:47 +0200

    allmost working version

--- createTranscriptDbPkgFromGtfFile.R
+++ createTranscriptDbPkgFromGtfFile.R
@@ -1,18 +1,33 @@
 library(GenomicFeatures)
 
+
 dataSource <- "ftp://ftp.ensemblgenomes.org/pub/metazoa/release-15/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.15.gtf.gz"
 
 #wget
 # unzip
 
-# chrominfo <- data.frame(chrom = c(''),
-#                         length=c(0),
-#                         is_circular=c(FALSE))
+library(BSgenome)
+library("BSgenome.Dmelanogaster.UCSC.dm3")
+sn <- do.call(rbind,strsplit(seqnames(seqinfo(Dmelanogaster)), "chr"))[,2]
+sn[8] <- "dmel_mitochondrion_genome"
+chrominfo <- data.frame(chrom=sn,
+                        length=seqlengths(seqinfo(Dmelanogaster)),
+                        is_circular=isCircular(seqinfo(Dmelanogaster)))
+rownames(chrominfo) <- NULL
 
-txdb <- makeTranscriptDbFromGFF("data/gtf/Drosophila_melanogaster.BDGP5.25.62.gtf", format="gtf", exonRankAttributeName="exon_number",  dataSource="ftp://ftp.ensemblgenomes.org/pub/metazoa/release-15/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.15.gtf.gz", species="Drosophila melanogaster"
-#                                 , chrominfo=chrominfo
-                                )
 
-makeTxDbPackage(txdb, "99.99", "FMI", "eee")
+txdb <- makeTranscriptDbFromGFF("data/gtf/Drosophila_melanogaster.BDGP5.25.62.gtf", 
+                                format="gtf", 
+                                exonRankAttributeName="exon_number",  
+                                gffGeneIdAttributeName="gene_name", 
+                                chrominfo=chrominfo,
+                                dataSource="ftp://ftp.ensemblgenomes.org/pub/metazoa/release-15/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.15.gtf.gz", 
+                                species="Drosophila melanogaster")
+
+pkgName <- "gtf.tx"
+PROVIDERVERSION = GenomicFeatures:::.getTxDbVersion(txdb)
+GenomicFeatures:::.getMetaDataValue(txdb, "Resource URL")
+
+pkg <- makeTxDbPackage(txdb=txdb, version="99.99", maintainer="FMImaintainer", author="FMIauthor", destDir=".")
 
 install.packages(pkgs="./TxDb.Dmelanogaster.UCSC.dm3.ensGene", repos=NULL)
